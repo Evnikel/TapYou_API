07b7f28036f3a062c49af7a2e9d4ddbaf0bbbe20
Evnikel <evnikel@gmail.com>
2024-04-28 23:12:05 +0300

    added logger.info

--- src/test/java/api/UserGenderAnd_ID.java
+++ src/test/java/api/UserGenderAnd_ID.java
@@ -2,6 +2,8 @@ package api;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.UserData;
 import org.testng.annotations.Test;
 
@@ -11,14 +13,15 @@ import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class UserGenderAnd_ID {
+    private static final Logger logger = LogManager.getLogger(UserGenderAnd_ID.class);
 
     @Test(dataProvider = "idData", dataProviderClass = UserData.class)
     public void testUserInfoById(String gender, int[] ids) {
         RestAssured.baseURI = UserData.BASE_URL;
         for (int id : ids) {
             try {
-                System.out.println("\u001B[34mResponses for gender " + gender + " and id " + id + "\u001B[0m");
-                System.out.println("\u001B[34m***********************************************\u001B[0m");
+                logger.info("\u001B[34mResponses for gender " + gender + " and id " + id + "\u001B[0m");
+                logger.info("\u001B[34m***********************************************\u001B[0m");
                 given()
                         .pathParam("id", id)
                         .when()
@@ -36,8 +39,8 @@ public class UserGenderAnd_ID {
                         .body("user.city", notNullValue())
                         .body("user.registrationDate", notNullValue());
             } catch (AssertionError e) {
-                System.out.println("\u001B[31mTest failed for gender " + gender + " and id " + id);
-                System.err.println("Error message: " + e.getMessage() + "\u001B[0m");
+                logger.info("\u001B[31mTest failed for gender " + gender + " and id " + id);
+                logger.info("Error message: " + e.getMessage() + "\u001B[0m");
             }
         }
     }
--- src/test/java/api/UserID.java
+++ src/test/java/api/UserID.java
@@ -2,6 +2,8 @@ package api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.UserData;
 import org.testng.annotations.Test;
 
@@ -9,11 +11,12 @@ import static io.restassured.RestAssured.given;
 import static org.example.UserData.USER_ENDPOINT;
 
 public class UserID {
+    private static final Logger logger = LogManager.getLogger(UserID.class);
 
     @Test(dataProvider = "idDataMultiple", dataProviderClass = UserData.class)
     public void testUserInfoByIdWithMultipleIds(int id) {
-        System.out.println("\u001B[34mResponses for id " + id + "\u001B[0m");
-        System.out.println("\u001B[34m***********************************************\u001B[0m");
+        logger.info("\u001B[34mResponses for id " + id + "\u001B[0m");
+        logger.info("\u001B[34m***********************************************\u001B[0m");
         Response response = given()
                 .pathParam("id", id)
                 .when()
@@ -29,19 +32,19 @@ public class UserID {
                     int userId = response.jsonPath().getInt("user.id");
 
                     if (userId == id) {
-                        System.out.println("Response body:");
-                        System.out.println(response.body().asString());
+                        logger.info("Response body:");
+                        logger.info(response.body().asString());
                     } else {
-                        System.out.println("\u001B[31mError: Requested id does not match the response id. Expected id: " + id + ", Actual id: " + userId + "\u001B[0m");
+                        logger.info("\u001B[31mError: Requested id does not match the response id. Expected id: " + id + ", Actual id: " + userId + "\u001B[0m");
                     }
                 } else {
-                    System.out.println("\u001B[31mError: Response body does not contain id\u001B[0m");
+                    logger.info("\u001B[31mError: Response body does not contain id\u001B[0m");
                 }
             } else {
-                System.out.println("\u001B[31mError status: " + response.body().asString() + "\u001B[0m");
+                logger.info("\u001B[31mError status: " + response.body().asString() + "\u001B[0m");
             }
         } else {
-            System.out.println("\u001B[31mError status: " + response.statusCode() + "\u001B[0m");
+            logger.info("\u001B[31mError status: " + response.statusCode() + "\u001B[0m");
         }
     }
 }
